91f4dae06976f6cf7967f5b0903a8d01794c191b
BrooklynSky <bharris47@cps.edu>
2025-03-17 14:22:21 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("work")
+
+let taste = document.querySelector(".taste");
+console.log(taste)
+let season = document.querySelector(".season");
+console.log(season)
+let quiz = document.querySelector(".quiz");
+console.log(quiz)
+
+quiz.onclick = function(){
+
+    console.log('work button');
+
+let answerTaste = taste.value;
+console.log(answerTaste);
+
+if (answerTaste === "salty"){
+    console.log("Yes");
+}
+else if(answerTaste==="sweet"){
+    console.log("Yes2")
+}
+
+
+    };
+
+
