805a28ff54a322d83bbfd9eb14d1bdd3f10eb7e7
BrooklynSky <bharris47@cps.edu>
2025-03-20 14:23:33 +0000

    v2

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 console.log("work")
 
-let taste = document.querySelector(".taste");
-console.log(taste)
-let season = document.querySelector(".season");
-console.log(season)
-let quiz = document.querySelector(".quiz");
-console.log(quiz)
-
-quiz.onclick = function(){
-
-    console.log('work button');
-
-let answerTaste = taste.value;
-console.log(answerTaste);
-
-if (answerTaste === "salty"){
-    console.log("Yes");
-}
-else if(answerTaste==="sweet"){
-    console.log("Yes2")
-}
+let button= document.querySelector("button");
 
 
-    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let taste = document.querySelector(".taste");
+// console.log(taste)
+// let season = document.querySelector(".season");
+// console.log(season)
+// let quiz = document.querySelector(".quiz");
+// console.log(quiz)
+
+// quiz.onclick = function(){
+
+//     console.log('work button');
+
+// let answerTaste = taste.value;
+// console.log(answerTaste);
+
+// if (answerTaste === "salty"){
+//     console.log("Yes");
+// }
+// else if(answerTaste==="sweet"){
+//     console.log("Yes2")
+// }
+
+
+//     };
 
 
